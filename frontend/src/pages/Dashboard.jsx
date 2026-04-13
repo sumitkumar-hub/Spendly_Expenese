6e508fa45cf8107c5653e2adc08f285f027af2d3
sumitkumar-hub <jaiswalsumit583@gmail.com>
2026-04-14 03:07:02 +0530

    .....

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useMemo, useState } from "react";
 import toast from "react-hot-toast";
-import { getExpenses } from "../api/expense.js";
+import { getxpenses } from "../api/expense.js";
 import { getBudget, setBudget } from "../api/budget.js";
 import { getCategoryBudgets, setCategoryBudget } from "../api/categoryBudget.js";
 import Layout from "../components/Layout";
